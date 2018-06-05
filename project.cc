16cd1863167baf768d9b94cf1825da2af6956a7f
Naman Garg <31505534+Naman-Garg@users.noreply.github.com>
2018-06-05 16:01:03 +0530

    Update project.cc

--- project.cc
+++ project.cc
@@ -1,12 +1,14 @@
 #include <bits/stdc++.h>
 #define key 17
 using namespace std;
+
 int main(){
      float a[130][5], d[130];
      char str[125], str1[125];
      float sl = 0.0, sw = 0.0, pl = 0.0, pw = 0.0, sl1 = 0.0, sw1 = 0.0, pl1 = 0.0, pw1 = 0.0, accuracy = 0.0;
      int i = 0, j = 0, k = 0, l = 0, m = 0, it = 0, count = 0, setosa = 0, versicolor = 0, virginica = 0;
      FILE *fp1, *fp2;
+
      fp1 = freopen("data_training.txt", "r",stdin);
      while(i < 120){
           fscanf(fp1, " %f %f %f %f %s ", &sl, &sw, &pl, &pw, str);
@@ -24,22 +26,21 @@ int main(){
                a[i][4] = 2;
           }
           i++;
-          //cout<<a[i][4]<<endl;
      }
-     /*for(i = 0; i < 120; i = i + 10)
-     {
-          cout << a[i][4] << endl;
-     }*/
+     
      fp2 = freopen("test.txt","r", stdin);
      it = 0;
      while(it < 30){
           fscanf(fp2, "%f %f %f %f %s ", &sl1, &sw1, &pl1, &pw1, str1);
           it++;
           setosa = virginica = versicolor = 0;
+          
+          //calculate the distance
           for (i = 0; i < 120; i++){
                d[i] = sqrt((pow((a[i][0] - sl1), 2)) + (pow((a[i][1] - sw1), 2)) + (pow((a[i][2] - pl1), 2)) + (pow((a[i][3] - pw1), 2)));
-               //cout << d[i] << endl ;
           }
+
+          //sort the distance acco. to increasing order
           for (j = 0; j < 120; j++){
                for(k = 0; k < 120-j-1; k++){
                     if(d[k] > d[k+1]){
@@ -52,10 +53,8 @@ int main(){
                     }
                }
           }
-          /*for (j = 0; j < 120; j++)
-          {
-               cout << d[j]<<endl;// <<endl;// " " << a[j][0] << " "  << a[j][1] << " " << a[j][2] << " " << a[j][3] << " " << a[j][4] << endl << endl;
-          }*/
+
+          //check the frequency of each flower
           for (m = 0; m < key; m++){
                if(a[m][4] == 0)
                     setosa++;
@@ -64,7 +63,8 @@ int main(){
                else if(a[m][4] == 2)
                     virginica++;
           }
-          //cout << setosa << " " << versicolor << " " << verginica << endl;
+
+          //increment the counter if it match with the flower
           if(setosa >= versicolor && setosa >= virginica){
                cout << "Setosa" << endl;
                if(strcmp(str1, "setosa") == 0)
@@ -80,8 +80,9 @@ int main(){
                if(strcmp(str1, "virginica") == 0)
                     count ++;
           }
-          //cout << count << endl;
      }
+
+     //calculate the accuracy
      accuracy = (count * 100) / 30;
      cout << accuracy << endl;
      fclose(fp1);
